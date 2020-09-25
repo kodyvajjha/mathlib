b3c1f091021905f7ad4e643632742fca6f2f9239
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2020-09-25 13:11:48 -0400

    Add the definition of join for the tangent bundle monad.

--- src/geometry/manifold/tangent_functor_monad.lean
+++ src/geometry/manifold/tangent_functor_monad.lean
@@ -13,12 +13,20 @@ variables {𝕜 : Type*} [nondiscrete_normed_field 𝕜]
 (M : Type*) [topological_space M] [charted_space H M] [smooth_manifold_with_corners I M]
 
 
-/-- The zero section is the pure of the tangent functor monad. -/
+/-- The zero section is the `pure` of the tangent bundle monad. -/
 def tangent_bundle.pure : M → tangent_bundle I M  :=
 λ x, ⟨x,0⟩
 
+/-- The zero section is a retraction of the projection. -/
 @[simp] lemma pure_section :
 ∀ x, (tangent_bundle.proj I M) (tangent_bundle.pure I M x) = x := λ _, rfl
+
+/-- The `join` operation of the tangent bundle monad.
+    ⟨x,x',v,v'⟩ ↦ ⟨x,v⟩ and ⟨x,x',v,v'⟩ ↦ ⟨x,v⟩ satisfies one of the two unit laws. Their sum satisfies both.  -/
+def tangent_bundle.join :
+ tangent_bundle I.tangent (tangent_bundle I M) → tangent_bundle I M :=
+λ ⟨⟨x, x'⟩, v, v'⟩, ⟨x, (v + x')⟩
+
 
 
 end tangent_bundle_monad
